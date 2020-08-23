5f371540c3d2d419473b706b666e8a3deb51ea00
Johannes Ganzenmüller <johannes.ganzenmueller@gmail.com>
2020-08-23 20:44:52 +0200

    Create pages for each post and add links at blog-page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 const Blog = () => {
   const data = useStaticQuery(
@@ -16,6 +16,9 @@ const Blog = () => {
                 timeToRead
                 excerpt
                 id
+                fields {
+                    slug
+                }
               }
             }
           }
@@ -28,7 +31,9 @@ const Blog = () => {
         {data.allMarkdownRemark.edges.map(edge => {
           return (
             <li key={edge.node.id}>
-              <h2>{edge.node.frontmatter.title}</h2>
+              <h2>
+                <Link to={`/blog/${edge.node.fields.slug}/`}>{edge.node.frontmatter.title}</Link>
+              </h2>
               <div>
                 <span>
                   Posted on {edge.node.frontmatter.date} <span> / </span>{" "}
@@ -36,6 +41,9 @@ const Blog = () => {
                 </span>
               </div>
               <p>{edge.node.excerpt}</p>
+              <div>
+                <Link to={`/blog/${edge.node.fields.slug}/`}>Read More</Link>
+              </div>
             </li>
           )
         })}
